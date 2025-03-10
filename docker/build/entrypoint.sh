f806f8b24faf503b641cbe8339ab50d12e5a7dd0
Giovanni Scafetta <giovanni.scaftta@gmail.com>
2025-03-10 17:21:29 +0100

    update entrypoint

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -f /data/repocreate.json ]; then
+  cp /repocreate_template.json /data/repocreate.json
+fi
+
 # Check if the CRON environment variable is set
 if [ -z "$CRON" ]; then
   echo "CRON is not set. Exiting."
@@ -8,6 +12,9 @@ fi
 
 # Create the crontab file dynamically
 echo "$CRON python /data/repocreate.py --run > /var/log/cron.log 2>&1" > /etc/crontabs/root
+
+# Add a cron job to clear the log file every 7 days
+echo "0 0 */7 * * echo '' > /var/log/cron.log" >> /etc/crontabs/root
 
 # Ensure the log file exists
 touch /var/log/cron.log
